effd5c18e1011f7512df40b950de2251112043f1
adamh36 <adamhammoud04@gmail.com>
2026-04-06 17:09:16 -0400

    chore: fix gitignore, remove cached files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from db.database import init_db
 
-# Initialize the database every time the app starts
 init_db()
 
-st.switch_page("pages/register.py")
+st.set_page_config(page_title="DriveShare", page_icon="🚗", layout="wide")
+
+if "user" not in st.session_state:
+    st.switch_page("pages/register.py")

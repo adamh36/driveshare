14a78a11d93cc3e5c90cf4dd141887dbc1fdbebe
adamh36 <adamhammoud04@gmail.com>
2026-04-10 16:21:05 -0400

    feat: migrate to Tkinter structure, add gui/ models/ folders

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from db.database import init_db
+from Patterns.session import session
+from Patterns.mediator import mediator
+from utils.theme import apply_theme
 
+st.set_page_config(page_title="DriveShare", page_icon="", layout="centered")
+
+apply_theme()
 init_db()
 
-st.set_page_config(page_title="DriveShare", page_icon="🚗", layout="wide")
-
-if "user" not in st.session_state:
-    st.switch_page("pages/register.py")
+if not session.is_logged_in():
+    st.switch_page("pages/login.py")
+else:
+    user = session.get_user()
+    mediator.navigate_after_login(user["role"])
